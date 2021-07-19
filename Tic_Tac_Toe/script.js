bc23ab48a9da32ebb099eee6e844fb5e4c7f8763
RistoLibera <qofgxnm7@gmail.com>
2021-07-19 22:16:39 +0900

    Update click event

--- Tic_Tac_Toe/script.js
+++ Tic_Tac_Toe/script.js
@@ -1,17 +1,18 @@
 // Restart game
 document.getElementById("restart").addEventListener('click', function(){location.reload()})
 
-const gameBoard = (() => {
+// Gameboard module
+const gameArea = (() => {
   const gridArea = document.getElementById("game-grid")
   const board = []
 
   const init = () => {
     for (let i = 0; i < 9; i++) {
       // Create gameboard
-      let gridItem = document.createElement("div") 
+      let gridItem = document.createElement("p") 
       gridItem.id = i + 1
       gridItem.classList.add("grid-item")
-      gridItem.innerHTML = i + 1
+      gridItem.innerHTML = " "
       gridArea.appendChild(gridItem)
       board.push(gridItem)
     }  
@@ -23,6 +24,55 @@ const gameBoard = (() => {
   }
 })()
 
+// Player factory
+const Player = (name) => {
+  let playerMoves = []
+
+  return {
+    name,
+    playerMoves
+  }
+}
+
+// Player form
+const playerForm = (() => {
+
+})()
+// Gameboard interation
+const gameBoard = (() => {
+  gameArea.init()
+
+  gameArea.board.forEach((p) => {
+    p.addEventListener("click", (e) => {
+      gameBlock = e.target
+      if (gameBlock.innerHTML === "X" || gameBlock.innerHTML === "O") {
+        alert("Taken!")
+      } else if (gameBlock.innerHTML === " " && gameFlow.turn === "player-one") {
+        gameBlock.innerHTML = "X"
+        gameFlow.turnNumber++
+        gameFlow.turn = "player-two"
+      } else {
+        gameBlock.innerHTML = "O"
+        gameFlow.turnNumber++
+        gameFlow.turn = "player-one"
+      }
+    })
+  })
+
+})()
+
+const gameFlow = (() => {
+  let turn = "player-one"
+  let turnNumber = 1
+  let players = []
+
+  return {
+    turn,
+    turnNumber,
+    players
+  }
+})()
 
 
-gameBoard.init()
+
+
