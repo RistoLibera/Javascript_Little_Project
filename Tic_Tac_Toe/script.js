9736b622c695be407314bc07fa1f93daaf57fed0
RistoLibera <qofgxnm7@gmail.com>
2021-07-19 17:38:33 +0900

    Update gameBoard module

--- Tic_Tac_Toe/script.js
+++ Tic_Tac_Toe/script.js
@@ -1,21 +1,28 @@
 // Restart game
 document.getElementById("restart").addEventListener('click', function(){location.reload()})
 
-function initializing() {
+const gameBoard = (() => {
   const gridArea = document.getElementById("game-grid")
+  const board = []
 
-  for (let i = 0; i < 9; i++) {
-    // Create gameboard
-    let gridItem = document.createElement("div") 
-    gridItem.id = i + 1
-    gridItem.value = i + 1
-    gridItem.classList.add("grid-item")
-    gridItem.innerHTML = i + 1
-    gridArea.appendChild(gridItem)
+  const init = () => {
+    for (let i = 0; i < 9; i++) {
+      // Create gameboard
+      let gridItem = document.createElement("div") 
+      gridItem.id = i + 1
+      gridItem.classList.add("grid-item")
+      gridItem.innerHTML = i + 1
+      gridArea.appendChild(gridItem)
+      board.push(gridItem)
+    }  
   }
-}
+
+  return {
+    init,
+    board
+  }
+})()
 
 
 
-
-initializing()
+gameBoard.init()
