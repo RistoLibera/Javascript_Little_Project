3cb13e3e0aab06bcc98543dd735d80c9eee39be6
RistoLibera <qofgxnm7@gmail.com>
2021-08-18 09:37:14 +0900

    Complete project: Battleship

---
+++ 9_battleship/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>No sure if my future company needs testing, well, i will just leave it like this</h1>
+    </div>
+  );
+}
+
+export default App;
